9eaffcd097238702f288c38ad120f0237468032a
Timo Reichl <thetredev@gmail.com>
2023-10-07 22:43:34 +0000

    scripts: Automate changelog creation
    Signed-off-by: Timo Reichl <thetredev@gmail.com>

---
+++ scripts/changelog.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+git fetch --tags
+
+first_tag=$(git tag --sort=-version:refname | head -n 2 | tail -1)
+second_tag=$(git tag --sort=-version:refname | head -n 1)
+
+mkdir -p bin
+
+echo "## What's Changed" > bin/changelog.md
+
+echo >> bin/changelog.md
+git log ${first_tag}..${second_tag} --pretty=format:"- %s: %h" >> bin/changelog.md
+echo >> bin/changelog.md
+echo >> bin/changelog.md
+
+go_module=$(go list -m | sed 's/\/v.*//')
+
+echo "**Full Changelog**: [\`${first_tag}..${second_tag}\`](https://${go_module}/compare/${first_tag}..${second_tag})" >> bin/changelog.md
+
+echo >> bin/changelog.md
+
+echo "## SHA256 Checksums" >> bin/changelog.md
+echo '```' >> bin/changelog.md
+
+for binary in $(ls -d -1 bin/xdeb-*); do
+    sha256sum ${binary} | sed 's/bin\///' >> bin/changelog.md
+done
+
+echo '```' >> bin/changelog.md
