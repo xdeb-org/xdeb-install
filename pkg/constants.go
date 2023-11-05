717872b01263cf91c30bee323227c3686f896226
Timo Reichl <thetredev@gmail.com>
2023-11-05 15:34:07 +0000

    pkg/constants: Adjust XDEB URLs after project transfer to xdeb-org
    Signed-off-by: Timo Reichl <thetredev@gmail.com>

--- pkg/constants.go
+++ pkg/constants.go
@@ -4,8 +4,8 @@ const APPLICATION_NAME = "xdeb-install"
 
 const LOG_MESSAGE_PREFIX = "[" + APPLICATION_NAME + "]"
 
-const XDEB_URL = "https://github.com/toluschr/xdeb/releases"
-const XDEB_MASTER_URL = "https://raw.githubusercontent.com/toluschr/xdeb/master"
+const XDEB_URL = "https://github.com/xdeb-org/xdeb/releases"
+const XDEB_MASTER_URL = "https://raw.githubusercontent.com/xdeb-org/xdeb/master"
 
 const XDEB_INSTALL_REPOSITORIES_TAG = "v1.1.1"
 const XDEB_INSTALL_REPOSITORIES_URL = "https://raw.githubusercontent.com/xdeb-org/xdeb-install-repositories"
