3afacc878aecddacf60839b133c01ae820b841e5
Timo Reichl <thetredev@gmail.com>
2023-12-09 21:18:02 +0000

    pkg/command_utils: More robust way to find a command in the argument list
    Signed-off-by: Timo Reichl <thetredev@gmail.com>

--- pkg/command_utils.go
+++ pkg/command_utils.go
@@ -7,6 +7,16 @@ import (
 	"strings"
 )
 
+func findArgumentIndex(command string, args ...string) int {
+	for i := 0; i < len(args); i++ {
+		if strings.HasSuffix(args[i], command) {
+			return i
+		}
+	}
+
+	return -1
+}
+
 func ExecuteCommand(workdir string, args ...string) error {
 	LogMessage("Executing command: %s ...", strings.Join(args, " "))
 
@@ -16,23 +26,21 @@ func ExecuteCommand(workdir string, args ...string) error {
 	command.Stderr = os.Stderr
 	command.Stdin = os.Stdin
 
-	commandIndex := 0
+	commandIndex := findArgumentIndex("xdeb", args...)
 
-	if args[0] == "sudo" {
-		commandIndex++
-	}
+	if commandIndex > -1 {
+		commandStringSlice := strings.Split(args[commandIndex], "/")
 
-	commandStringSlice := strings.Split(args[commandIndex], "/")
-
-	// Add xdeb-specific environment to xdeb command
-	if commandStringSlice[len(commandStringSlice)-1] == "xdeb" {
-		for _, environmentVariable := range os.Environ() {
-			if strings.HasPrefix(environmentVariable, "XDEB_") {
-				command.Env = append(command.Env, environmentVariable)
+		// Add xdeb-specific environment to xdeb command
+		if commandStringSlice[len(commandStringSlice)-1] == "xdeb" {
+			for _, environmentVariable := range os.Environ() {
+				if strings.HasPrefix(environmentVariable, "XDEB_") {
+					command.Env = append(command.Env, environmentVariable)
+				}
 			}
-		}
 
-		command.Env = append(command.Env, fmt.Sprintf("XDEB_PKGROOT=%s", workdir))
+			command.Env = append(command.Env, fmt.Sprintf("XDEB_PKGROOT=%s", workdir))
+		}
 	}
 
 	return command.Run()
