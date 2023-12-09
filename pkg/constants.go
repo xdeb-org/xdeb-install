b17a593a75bea478eb7095c2492d94be1e19df11
Timo Reichl <thetredev@gmail.com>
2023-12-09 22:42:11 +0000

    constants: Set XDEB_INSTALL_REPOSITORIES_TAG to v1.1.2
    This tag uses a backup mirror for the `linuxmint.com` provider due to
    the official one being down sporadically.
    
    See xdeb-install-repositories commit c5ebe9f9931bfec09a305bd8581b8a09bb1beeb7 for details.
    https://github.com/xdeb-org/xdeb-install-repositories/commit/c5ebe9f9931bfec09a305bd8581b8a09bb1beeb7
    
    Signed-off-by: Timo Reichl <thetredev@gmail.com>

--- pkg/constants.go
+++ pkg/constants.go
@@ -7,5 +7,5 @@ const LOG_MESSAGE_PREFIX = "[" + APPLICATION_NAME + "]"
 const XDEB_URL = "https://github.com/xdeb-org/xdeb/releases"
 const XDEB_MASTER_URL = "https://raw.githubusercontent.com/xdeb-org/xdeb/master"
 
-const XDEB_INSTALL_REPOSITORIES_TAG = "v1.1.1"
+const XDEB_INSTALL_REPOSITORIES_TAG = "v1.1.2"
 const XDEB_INSTALL_REPOSITORIES_URL = "https://raw.githubusercontent.com/xdeb-org/xdeb-install-repositories"
