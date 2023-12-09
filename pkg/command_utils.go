713a0fb590901eeb0ca2f21d6090b14be938eb2a
Timo Reichl <thetredev@gmail.com>
2023-12-09 21:08:10 +0000

    Fix issue #15: Add xdeb-specific environment to xdeb command
    Signed-off-by: Timo Reichl <thetredev@gmail.com>

--- pkg/command_utils.go
+++ pkg/command_utils.go
@@ -16,7 +16,22 @@ func ExecuteCommand(workdir string, args ...string) error {
 	command.Stderr = os.Stderr
 	command.Stdin = os.Stdin
 
-	if args[0] == "xdeb" {
+	commandIndex := 0
+
+	if args[0] == "sudo" {
+		commandIndex++
+	}
+
+	commandStringSlice := strings.Split(args[commandIndex], "/")
+
+	// Add xdeb-specific environment to xdeb command
+	if commandStringSlice[len(commandStringSlice)-1] == "xdeb" {
+		for _, environmentVariable := range os.Environ() {
+			if strings.HasPrefix(environmentVariable, "XDEB_") {
+				command.Env = append(command.Env, environmentVariable)
+			}
+		}
+
 		command.Env = append(command.Env, fmt.Sprintf("XDEB_PKGROOT=%s", workdir))
 	}
 
